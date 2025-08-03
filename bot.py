252361642060430109cff4bd34a16bcdcbc90fc1
karaman56 <166434324+karaman56@users.noreply.github.com>
2025-08-03 22:52:14 +0500

    исправил Logger  убрал лишнии строки
    исправил Logger  убрал лишнии строки

--- bot.py
+++ bot.py
@@ -5,11 +5,10 @@ import logging
 from dotenv import load_dotenv
 
 
-
+logger = logging.getLogger(__name__)
 
 
 def main():
-    logger = logging.getLogger(__name__)
     logging.basicConfig(level=logging.ERROR)
     logger.setLevel(logging.INFO)
 
